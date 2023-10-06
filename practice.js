bb04738213596ccafc546cb94f13f15206751098
ggmmmm45 <130798916+ggmmmm45@users.noreply.github.com>
2023-10-06 17:33:40 -0300

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,26 @@
+// ADICIONE SUS LINKS FIREBASE AQUI
+var firebaseConfig = {
+        apiKey: "AIzaSyCYXiTh85QfTzkY0hkYZYjhODf8lVbtKgU",
+        authDomain: "kwitterprofthays.firebaseapp.com",
+        databaseURL: "https://kwitterprofthays-default-rtdb.firebaseio.com",
+        projectId: "kwitterprofthays",
+        storageBucket: "kwitterprofthays.appspot.com",
+        messagingSenderId: "182007721289",
+        appId: "1:182007721289:web:f20b13c099fe4e9a507e98"
+      };
+      
+      
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+      
+      // programe a função addUser():
+      function addUser()
+      {
+      // Obtenha o nome do usuário a partir da input box, com a id da input box. E,armazene-o dentro de uma variável.
+
+      // adicione o valor dessa variável ao firebase: 
+
+          
+        
+      }
+      
